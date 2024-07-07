5a7488a16624c149d27163a43b3499011516a6d8
alvarixxs <alvarodesants@gmail.com>
2024-07-07 21:30:08 +0200

    api endpoint for login now returns user id, needed by client to fetch liked products and products in cart

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -33,7 +33,7 @@ router.post('/', async (request, response) => {
 
   response
     .status(200)
-    .send({ token, username: user.username, name: user.name })
+    .send({ token, username: user.username, name: user.name, id: user.id })
 })
 
 module.exports = router
